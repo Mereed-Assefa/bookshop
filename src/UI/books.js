6f062f3387c8c9c7c716485d78fc12730eff83f1
Mereed Assefa <mrdassefa@gmail.com>
2022-04-11 09:06:53 +0300

    book card

---
+++ src/UI/books.js
@@ -0,0 +1,11 @@
+export default function books() {
+  return (
+    <div>
+      <div className="book-image"></div>
+      <div className="book-info">
+        <div className="book-title"> </div>
+        <div className="book-author"> </div>
+      </div>
+    </div>
+  );
+}
