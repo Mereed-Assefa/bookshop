5f6d793ddb9f9fe8f2ed941b3ab43ab6632475ad
Mereed Assefa <mrdassefa@gmail.com>
2022-04-15 10:56:31 +0300

    my commit

--- src/UI/books.js
+++ src/UI/books.js
@@ -1,10 +1,51 @@
-export default function books() {
+import dertogadaCover from "../images/dertogada.jpeg";
+export default function Books() {
   return (
     <div>
-      <div className="book-image"></div>
-      <div className="book-info">
-        <div className="book-title"> </div>
-        <div className="book-author"> </div>
+      <div id="book-holder">
+        <div id="book-section">
+          <div className="book-image">
+            <img src={dertogadaCover} />
+          </div>
+
+          <div className="book-info">
+            <div className="book-title">
+              <h3>Dertogada</h3>
+            </div>
+            <div className="book-author">
+              <h4>Yismake Worku</h4>{" "}
+            </div>
+          </div>
+        </div>
+        <div id="book-section">
+          <div className="book-image">
+            <img src={dertogadaCover} />
+          </div>
+
+          <div className="book-info">
+            <div className="book-title">
+              <h3>Dertogada</h3>
+            </div>
+            <div className="book-author">
+              <h4>Yismake Worku</h4>{" "}
+            </div>
+          </div>
+        </div>
+
+        <div id="book-section">
+          <div className="book-image">
+            <img src={dertogadaCover} />
+          </div>
+
+          <div className="book-info">
+            <div className="book-title">
+              <h3>Dertogada</h3>
+            </div>
+            <div className="book-author">
+              <h4>Yismake Worku</h4>{" "}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
