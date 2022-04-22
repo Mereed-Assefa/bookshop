a39ed8fabb0a9b6d45d4a8b68f56bfcfb34e87cf
Mereed Assefa <mrdassefa@gmail.com>
2022-04-22 12:43:31 +0300

    b

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import Navbar from "./UI/navbar";
 import Books from "./UI/books";
 import MediaCard from "./UI/MediaCard";
+import Bookcard from "./UI/Bookcard";
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <Bookcard />
       <div className="book-card">
         <MediaCard />
         <MediaCard />
--- src/UI/Bookcard.js
+++ src/UI/Bookcard.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import dertogadaCover from "../images/dertogada.jpeg";
 
 export class Bookcard extends Component {
   render() {
-    return <div>Bookcard</div>;
+    return (
+      <div>
+        <div className="main-container">
+          <div className="book-image">
+            <img src={dertogadaCover} />
+          </div>
+          <div className="book-info">
+            <div className="book-description">
+              <h1>Title</h1>
+              <h2>Author</h2>
+              <h4>This is book description bla bla bla</h4>
+            </div>
+            <div className="book-buttons">
+              <button>Buy</button>
+              <button>Read More</button>
+              <button>like</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
